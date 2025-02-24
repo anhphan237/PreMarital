ef2905f8a45d9deda9a7d69afe8fd7f8be7ec370
anhphan237 <anh713988@gmail.com>
2025-02-24 23:43:45 +0700

    QA - Fix pagination #3

--- src/main/java/com/example/premarital/services/impl/UserServiceImpl.java
+++ src/main/java/com/example/premarital/services/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.premarital.services.impl;
 
+
 import com.example.premarital.common.pagination.PaginationRequest;
 import com.example.premarital.common.pagination.PaginationUtils;
 import com.example.premarital.common.pagination.PagingResult;
@@ -10,11 +11,13 @@ import com.example.premarital.mappers.UserMapper;
 import com.example.premarital.models.User;
 import com.example.premarital.repositories.UserRepository;
 import com.example.premarital.services.UserService;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.function.Function;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -28,19 +31,48 @@ public class UserServiceImpl implements UserService {
         this.userMapper = userMapper;
     }
 
+//    @Override
+//    public PagingResult<UserDTO> getUsers(PaginationRequest request) {
+//        final Pageable pageable = PaginationUtils.getPageable(request);
+//        final Page<User> entities = userRepository.findAll(pageable);
+//        final List<UserDTO> entitiesDto = entities.stream().map(userMapper::toDTO).toList();
+//        return new PagingResult<>(
+//                entitiesDto,
+//                entities.getTotalPages(),
+//                entities.getTotalElements(),
+//                entities.getSize(),
+//                entities.getNumber(),
+//                entities.isEmpty()
+//        );
+//    }
+
+
     @Override
-    public PagingResult<UserDTO> getUsers(PaginationRequest request) {
-        final Pageable pageable = PaginationUtils.getPageable(request);
-        final Page<User> entities = userRepository.findAll(pageable);
-        final List<UserDTO> entitiesDto = entities.stream().map(userMapper::toDTO).toList();
-        return new PagingResult<>(
-                entitiesDto,
-                entities.getTotalPages(),
-                entities.getTotalElements(),
-                entities.getSize(),
-                entities.getNumber(),
-                entities.isEmpty()
-        );
+    public Page<UserDTO> getUsers(Pageable pageable) {
+        Page<User> entities = userRepository.findAll(pageable);
+        Page<UserDTO> dtoPage = entities.map(new Function<User, UserDTO>() {
+            @Override
+            public UserDTO apply(User entity) {
+                UserDTO dto = new UserDTO();
+                // Conversion logic
+                dto.setId(entity.getId());
+                dto.setFirstName(entity.getFirstName());
+                dto.setLastName(entity.getLastName());
+                dto.setEmail(entity.getEmail());
+                dto.setRoleId(entity.getRole().getId());
+                dto.setUsername(entity.getUsername());
+                dto.setCity(entity.getCity());
+                dto.setCountry(entity.getCountry());
+                dto.setPassword(entity.getPassword());
+                dto.setPostalCode(entity.getPostalCode());
+                dto.setState(entity.getState());
+                dto.setRoleId(entity.getRole().getId());
+                dto.setStreet(entity.getStreet());
+
+                return dto;
+            }
+        });
+        return dtoPage;
     }
 
     @Override
