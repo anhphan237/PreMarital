a4759ab41d1028b373864decd8bc999407ddde9e
anhphan237 <anh713988@gmail.com>
2025-03-05 00:32:27 +0700

    Update quiz

--- src/main/java/com/example/premarital/mappers/QuizMapper.java
+++ src/main/java/com/example/premarital/mappers/QuizMapper.java
@@ -1,4 +1,9 @@
 package com.example.premarital.mappers;
 
+import com.example.premarital.dtos.QuizDTO;
+import com.example.premarital.models.Quiz;
+
 public interface QuizMapper {
+    QuizDTO toDTO(Quiz quiz);
+    Quiz toEntity(QuizDTO quizDTO);
 }
--- src/main/java/com/example/premarital/services/impl/QuizServiceImpl.java
+++ src/main/java/com/example/premarital/services/impl/QuizServiceImpl.java
@@ -3,27 +3,46 @@ package com.example.premarital.services.impl;
 import com.example.premarital.common.pagination.PaginationRequest;
 import com.example.premarital.common.pagination.PagingResult;
 import com.example.premarital.dtos.QuizDTO;
+import com.example.premarital.dtos.QuizQuestionDTO;
+import com.example.premarital.mappers.QuizMapper;
 import com.example.premarital.models.Quiz;
+import com.example.premarital.models.QuizQuestion;
 import com.example.premarital.services.QuizService;
 import com.example.premarital.repositories.QuizRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
 
 @Service
 public class QuizServiceImpl implements QuizService {
     private final QuizRepository quizRepository;
+    private final QuizMapper quizMapper;
 
-    public QuizServiceImpl(QuizRepository quizRepository) {
+    public QuizServiceImpl(QuizRepository quizRepository, QuizMapper quizMapper) {
         this.quizRepository = quizRepository;
+        this.quizMapper = quizMapper;
     }
 
     @Override
-    public PagingResult<QuizDTO> getQuizzes(PaginationRequest request) {
-        return null;
+    public Page<QuizDTO> getQuizzes(Pageable pageable) {
+        Page<Quiz> entities = quizRepository.findAll(pageable);
+        Page<QuizDTO> dtoPage = entities.map(new Function<Quiz, QuizDTO>() {
+
+            @Override
+            public QuizDTO apply(Quiz quiz) {
+                QuizDTO dto = quizMapper.toDTO(quiz);
+                return dto;
+            }
+        });
+        return dtoPage;
     }
 
     @Override
-    public QuizDTO createQuiz(QuizDTO dto) {
-        return null;
+    public void createQuiz(QuizDTO dto) {
+        Quiz quiz = quizMapper.toEntity(dto);
+        quizRepository.save(quiz);
     }
 
     @Override
