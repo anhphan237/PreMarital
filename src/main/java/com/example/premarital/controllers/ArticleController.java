f204809c8d0947fc2a6c085e0dddfff0012906a7
anhphan237 <anh713988@gmail.com>
2025-03-07 14:10:05 +0700

    Update article

--- src/main/java/com/example/premarital/controllers/ArticleController.java
+++ src/main/java/com/example/premarital/controllers/ArticleController.java
@@ -2,6 +2,7 @@ package com.example.premarital.controllers;
 
 import com.example.premarital.dtos.ArticleDTO;
 import com.example.premarital.dtos.WalletDTO;
+import com.example.premarital.dtos.WithdrawRequestDTO;
 import com.example.premarital.services.ArticleService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -21,7 +22,7 @@ public class ArticleController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<ArticleDTO>> getUsers(
+    public ResponseEntity<Page<ArticleDTO>> getArticles(
             @RequestParam(required = false) Integer page,
             @RequestParam(required = false) Integer size,
             @RequestParam(required = false) String sort,
@@ -41,5 +42,27 @@ public class ArticleController {
     public ResponseEntity<String> createArticle(@RequestBody ArticleDTO article){
         articleService.createArticle(article);
         return new ResponseEntity<>("Article created successfully",HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ArticleDTO> findArticleById(@PathVariable Long id){
+        ArticleDTO article = articleService.getArticleById(id);
+        return new ResponseEntity<>(article, article != null ? HttpStatus.OK : HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteArticle(@PathVariable Long id) {
+        boolean deleted = articleService.deleteArticleById(id);
+        return deleted
+                ? ResponseEntity.ok("Article deleted successfully")
+                : ResponseEntity.status(HttpStatus.NOT_FOUND).body("Article not found");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateArticle(@PathVariable Long id, @RequestBody ArticleDTO updatedArticle) {
+        boolean updated = articleService.updateArticle(id, updatedArticle);
+        return updated
+                ? ResponseEntity.ok("Article updated successfully")
+                : ResponseEntity.status(HttpStatus.NOT_FOUND).body("Article not found");
     }
 }
